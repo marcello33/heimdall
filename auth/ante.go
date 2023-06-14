cbd4c020733a9a2d8874457689a5d6166d4d5851
Raneet Debnath <raneetdebnath10@gmail.com>
2023-06-14 17:36:56 +0530

    add log

--- auth/ante.go
+++ auth/ante.go
@@ -235,7 +235,8 @@ func processSig(
 
 		copy(pk[:], p[:])
 
-		helper.Logger.Info("ADDRESS", "account", pk.Address())
+		helper.Logger.Info("DERIVED ADDRESS", "pk", pk.Address())
+		helper.Logger.Info("ACC ADDRESS", "acc", acc.GetAddress())
 		if !bytes.Equal(acc.GetAddress().Bytes(), pk.Address().Bytes()) {
 			return nil, sdk.ErrUnauthorized("signature verification failed; verify correct account sequence and chain-id").Result()
 		}
